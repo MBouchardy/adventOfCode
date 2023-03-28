097b8b5dc68a4414049a70815dd942f55a6b104f
Marc BOUCHARDY <bouchardy.marc@gmail.com>
2023-03-28 14:57:08 +0200

    nouvelle structure de fichier

--- src/main/java/year2022/day_2/Main.java
+++ src/main/java/year2022/day_2/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package year2022.day_2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("yoooh les boys");
     }
 }
