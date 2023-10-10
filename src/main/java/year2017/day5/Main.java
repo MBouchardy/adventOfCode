57758a9252a868cdba011dc8b4da7c4d9cd90a5e
Marc BOUCHARDY <bouchardy.marc@gmail.com>
2023-10-10 11:46:15 +0200

    corrections day 5 2017

--- src/main/java/year2017/day5/Main.java
+++ src/main/java/year2017/day5/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> instructions = new ArrayList<>();
-        List<Integer> instructionsPart2 = new ArrayList<>();
+
         // Read input
         try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/inputs/day_05_2017.txt"))) {
             String line;
@@ -17,50 +17,46 @@ public class Main {
             while ((line = reader.readLine()) != null) {
                 int instruction = Integer.parseInt(line);
                 instructions.add(instruction);
-                instructionsPart2.add(instruction);
             }
-
+            List<Integer> instructionsPart2 = new ArrayList<>(instructions);
             // Read input[i] = n    countStep += 1
             // If i+1 <= input.size
             int countStep = 0;
-            for (int i = 0; i < instructions.size(); ) {
+            int i = 0;
+            while (i < instructions.size()) {
                 int instruction = instructions.get(i);
-                countStep += 1;
-                if (instruction == 0) {
-                    instructions.set(i, instructions.get(i) + 1);
-                } else {
-                    instructions.set(i, instructions.get(i) + 1);
+                countStep++;
+                instructions.set(i, instructions.get(i) + 1);
+                if (instruction != 0) {
                     i += instructions.get(i) - 1;
-                    if ( i > instructions.size()-1) {
-                        System.out.println("Part 1: il faut " + countStep + " étapes");
-
+                    // If exit from left
+                    if (i < 0) {
+                        break;
                     }
+
                 }
             }
+            System.out.println("Part 1: il faut " + countStep + " étapes");
+
             int countStepPart2 = 0;
-            for (int i = 0; i < instructionsPart2.size(); ) {
-                int instructionPart2 = instructionsPart2.get(i);
-                countStepPart2 += 1;
-                if (instructionPart2 == 0) {
-                    instructionsPart2.set(i, instructionsPart2.get(i) + 1);
+            int y = 0;
+            while (y < instructionsPart2.size()) {
+                int instructionPart2 = instructionsPart2.get(y);
+                countStepPart2++;
+                if (instructionsPart2.get(y) < 3 || instructionPart2 == 0) {
+                    instructionsPart2.set(y, instructionsPart2.get(y) + 1);
+                    y += instructionsPart2.get(y) - 1;
                 } else {
-                    if (instructionsPart2.get(i)<3){
-                        instructionsPart2.set(i, instructionsPart2.get(i) +1);
-                        i += instructionsPart2.get(i) - 1;
-                    }
-                    else {
-                        instructionsPart2.set(i, instructionsPart2.get(i) -1);
-                        i += instructionsPart2.get(i) + 1;
-                    }
-
-                    }
-                if (i > instructionsPart2.size() - 1) {
-                    System.out.println("Part 2: il faut " + countStepPart2 + " étapes");
+                    instructionsPart2.set(y, instructionsPart2.get(y) - 1);
+                    y += instructionsPart2.get(y) + 1;
+                }
+                // Test if exit from left
+                if (y < 0) {
+                    break;
                 }
             }
-                // read input[i+n]  countStep += 1
-            // input[i] +=1
-            // else print countStep
+            System.out.println("Part 2: il faut " + countStepPart2 + " étapes");
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
